03833de639d955d49eca125371b665931ad8ed91
@Jerin <133591611+jerinakter99@users.noreply.github.com>
2024-12-17 16:57:54 +0600

    gbdgd

---
+++ SjHProject/Controllers/EducatsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SjHProject.Controllers
+{
+    public class EducatsController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+       
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Furniture()
+        {
+            return View();
+        }
+        public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+       
+        
+    }
+}
---
+++ SjHProject/DataModel/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SjHProject.DataModel
+{
+    public class Contact
+    {
+        [Key]
+        public int ID { get; set; }
+        public string?Name { get; set; }
+        public string?Phone{ get; set; }
+        public string?Email{ get; set; }
+        public string?Massage{ get; set; }
+    }
+}
